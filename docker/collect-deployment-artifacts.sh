6c768609f89bf08c213bcdd6746db1221ea21424
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2020-02-18 12:43:11 +0100

    update
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

---
+++ docker/collect-deployment-artifacts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Helper script to collect executables, config and libraries needed for minimal docker
+
+mkdir /deploy
+cp /kuksa.val/build/src/w3c-visserver /deploy
+
+ldd /kuksa.val/build/src/w3c-visserver | grep "=>" | awk '{print $3}' |   xargs -I '{}' cp -v '{}' /deploy
+
+mkdir /config
+mkdir /config/certs
+cp  /kuksa.val/build/src/vss_rel_2.0.json /config/
+cp  /kuksa.val/build/src/Server.key /config/certs/kuksa.val.key
